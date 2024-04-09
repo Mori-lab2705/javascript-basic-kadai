50b88b6488e8b2f9d1fd9e155f6c0ccb4129de98
Mori-lab2705 <morihiro2705@gamil.com>
2024-04-09 23:05:59 +0900

    課題提出

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const addBtn = document.getElementById('btn');
+
+
+addBtn.addEventListener('click', () => {
+    const text = document.getElementById('text');
+    text.textContent = 'ボタンをクリックしました';
+});
